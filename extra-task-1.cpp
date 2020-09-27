d35d8152a2103f31488f7e00fb758433635f7289
Geluos <rodion.bezuglov@yandex.ru>
2020-09-27 22:40:11 +0300

    fix

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,11 +1,16 @@
+#include <assert.h>
+#include <cmath>
+
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
     return time_2 - time_1;
-    
-    /*    
+
+    /*
         Return the number of seconds later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> seconds_difference(1800.0, 3600.0)
         1800.0
 
@@ -22,11 +27,11 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
-	return seconds_difference(time_2 - time_1)/3600;
+	return seconds_difference(time_2, time_1)/3600;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
-            
+
         >>> hours_difference(1800.0, 3600.0)
         0.5
 
@@ -43,6 +48,8 @@ double hours_difference(double time_1, double time_2)
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+	assert(seconds >= 0 && seconds < 60);
+	assert(minutes >= 0 && minutes < 60);
 	return hours + minutes/60 + seconds/3600;
     /*
         Return the total number of hours in the specified number
@@ -63,8 +70,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
-	assert((0 <= seconds) && (seconds <= 60) && (0 <= minutes) && (minutes <= 60));
-	return hours % 24;
+	return fmod(hours, 24);
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -73,22 +79,22 @@ double to_24_hour_clock(double hours)
 
         >>> to_24_hour_clock(24)
         0
-        
+
         >>> to_24_hour_clock(48)
         0
-        
+
         >>> to_24_hour_clock(25)
         1
-        
+
         >>> to_24_hour_clock(4)
         4
-        
+
         >>> to_24_hour_clock(28.5)
         4.5
-        
+
         You may wish to inspect various function in <cmath> to work
         with integer and fractional part of a hours separately.
-        
+
     */
 }
 
@@ -97,7 +103,7 @@ double to_24_hour_clock(double hours)
         * get_hours
         * get_minutes
         * get_seconds
-    They are used to determine the hours part, minutes part and seconds part 
+    They are used to determine the hours part, minutes part and seconds part
     of a time in seconds. E.g.:
 
     >>> get_hours(3800)
@@ -109,7 +115,7 @@ double to_24_hour_clock(double hours)
     >>> get_seconds(3800)
     20
 
-    In other words, if 3800 seconds have elapsed since midnight, 
+    In other words, if 3800 seconds have elapsed since midnight,
     it is currently 01:03:20 (hh:mm:ss).
 */
 
@@ -123,19 +129,19 @@ double time_to_utc(int utc_offset, double time)
 
         >>> time_to_utc(+0, 12.0)
         12.0
- 
+
         >>> time_to_utc(+1, 12.0)
         11.0
- 
+
         >>> time_to_utc(-1, 12.0)
         13.0
- 
+
         >>> time_to_utc(-11, 18.0)
         5.0
- 
+
         >>> time_to_utc(-1, 0.0)
         1.0
- 
+
         >>> time_to_utc(-1, 23.0)
         0.0
     */
@@ -148,25 +154,25 @@ double time_from_utc(int utc_offset, double time)
 
         >>> time_from_utc(+0, 12.0)
         12.0
- 
+
         >>> time_from_utc(+1, 12.0)
         13.0
- 
+
         >>> time_from_utc(-1, 12.0)
         11.0
- 
+
         >>> time_from_utc(+6, 6.0)
         12.0
- 
+
         >>> time_from_utc(-7, 6.0)
         23.0
- 
+
         >>> time_from_utc(-1, 0.0)
         23.0
- 
+
         >>> time_from_utc(-1, 23.0)
         22.0
- 
+
         >>> time_from_utc(+1, 23.0)
         0.0
     */
